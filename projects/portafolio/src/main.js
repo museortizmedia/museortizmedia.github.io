cc36c27661149a2b3cb079e0486c4742fc6de73f
Talentum GameDev 13 <143554849+talentumgamedev13@users.noreply.github.com>
2024-06-07 00:25:33 -0500

    init vite react portafolio

---
+++ projects/portafolio/src/main.js
@@ -0,0 +1,20 @@
+// Configure dark mode strategy
+tailwind.config = {
+    darkMode: 'class', /* 'class' or 'media', we use 'class' to enable dark mode manually */
+}
+// Preload dark mode in local or windows match
+if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark')
+}
+// Activa el dark mode
+const ActiveDarkMood = ()=>{
+    localStorage.theme = 'dark';
+    document.documentElement.classList.add('dark');
+}
+// Desactiva el dark mode
+const DisactiveDarkMood = ()=>{
+    localStorage.theme = 'ligth';
+    document.documentElement.classList.remove('dark');
+}
