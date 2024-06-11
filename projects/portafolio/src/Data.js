8539a36576d69dd0f03fd5a75eb290fac57756de
Talentum GameDev 13 <museortiz@gmail.com>
2024-06-11 01:20:21 -0500

    Create Data.js
    - Add Data Local setting (in future this data can be consumed by a endpoint)

---
+++ projects/portafolio/src/Data.js
@@ -0,0 +1,128 @@
+const PageData = [
+  {
+    link: "#",
+    label: "Inicio",
+  },
+  {
+    link: "#",
+    label: "About"
+  },
+  {
+    link: "#",
+    label: "Works"
+  },
+  {
+    link: "#",
+    label: "Contact"
+  }
+]
+const TagLineData = "Content in local";
+const ProfileData = {
+  Nombre: "Diego Ortiz",
+  Apodo: "Muse Coder",
+  NombreCompleto: "Diego Ortiz Hurtado",
+  Rol: "Programador",
+  SortBio: "Soy un desarrollador de aplicativos interactivos.",
+  Bio: "Sit amet luctussd fav venenatis, lectus magna fringilla inis urna, porttitor rhoncus dolor purus non enim praesent in elementum sahas facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etisam dignissim diam quis enim lobortis viverra orci sagittis eu volutpat odio facilisis mauris sit. Scelerisque fermentum duisi faucibus in ornare quam sisd sit amet luctussd fav venenatis, lectus magna fringilla zac urna, porttitor rhoncus dolor purus non enim praesent cuz elementum sahas facilisis leot.",
+  ImageSrc: "https://media.licdn.com/dms/image/D4E03AQHD4cTeI7u7Hw/profile-displayphoto-shrink_800_800/0/1712670440594?e=1723680000&v=beta&t=jLSlBcNtD69829yHaaUTXMjM1cxddFvFocOKUog5FYQ",
+  State: 1
+};
+const EducationData = [
+  {
+    Name: "Tecnología en Produccion de Multimedia",
+    InitAge: "2016",
+    FinishAge: "2018",
+    Insitution: "Servicio Nacional de Aprendizaje SENA",
+    Desc: "Sit amet luctussd fav venenatis, lectus magna fringilla inis urna, porttitor rhoncus dolor purus non enim praesent in elementum sahas facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etisam dignissim diam quis enim lobortis viverra orci sagittis eu volutpat odio facilisis mauris sit.",
+    link: "",
+  },
+  {
+    Name: "Ingeniería en Multimedia",
+    InitAge: "2018",
+    FinishAge: "Actualidad",
+    Insitution: "Universidad Autónoma de Occidente",
+    Desc: "Sit amet luctussd fav venenatis, lectus magna fringilla inis urna, porttitor rhoncus dolor purus non enim praesent in elementum sahas facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etisam dignissim diam quis enim lobortis viverra orci sagittis eu volutpat odio facilisis mauris sit.",
+    link: "",
+  },
+];
+
+const ExperienceData = [
+  {
+    Cargo: "Desarrollador Vue",
+    InitAge: "Enero 2022",
+    FinishAge: "Agosto 2022",
+    Insitution: "OpenCode SAS",
+    Desc: "Sit amet luctussd fav venenatis, lectus magna fringilla inis urna, porttitor rhoncus dolor purus non enim praesent in elementum sahas facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etisam dignissim diam quis enim lobortis viverra orci sagittis eu volutpat odio facilisis mauris sit.",
+    link: "",
+  },
+  {
+    Cargo: "Desarrollador Unity",
+    InitAge: "2022",
+    FinishAge: "Actualidad",
+    Insitution: "Corporación Talentum",
+    Desc: "Sit amet luctussd fav venenatis, lectus magna fringilla inis urna, porttitor rhoncus dolor purus non enim praesent in elementum sahas facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etisam dignissim diam quis enim lobortis viverra orci sagittis eu volutpat odio facilisis mauris sit.",
+    link: "",
+  },
+];
+const SkillsData = [
+  {
+    Nombre: "JavaScript",
+    Dominio: "85",
+    Desc: "Non enim praesent",
+  },
+  {
+    Nombre: "Figma",
+    Dominio: "95",
+    Desc: "Non enim praesent",
+  },
+  {
+    Nombre: "React",
+    Dominio: "81",
+    Desc: "Non enim praesent",
+  },
+  {
+    Nombre: "Vue",
+    Dominio: "78",
+    Desc: "Non enim praesent",
+  },
+  {
+    Nombre: "Adobe",
+    Dominio: "90",
+    Desc: "Non enim praesent",
+  },
+  {
+    Nombre: "Unity",
+    Dominio: "87",
+    Desc: "Non enim praesent",
+  },
+];
+const OtherData = [
+  {
+    Nombre: "Unity Certification",
+    Date: "14 May 2020",
+    Desc: "Non enim praesent",
+    Image: "",
+    link: "",
+  },
+];
+const SocialData = [
+  {
+    Red: "LinkedIn",
+    Profile: "",
+    Link: "",
+    Image: "",
+    Desc: "Non enim praesent",   
+  },
+];
+
+
+export { 
+  PageData,
+  TagLineData,
+  ProfileData,
+  EducationData,
+  ExperienceData,
+  SkillsData,
+  OtherData,
+  SocialData,
+};
