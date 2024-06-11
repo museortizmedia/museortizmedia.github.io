bd2b690bf9c7ad0e730c91b3edd4859a13dd83b6
Talentum GameDev 13 <museortiz@gmail.com>
2024-06-11 01:23:34 -0500

    Create TailwindComponents.js
    - Espetial desing components in a object by scalable use

---
+++ projects/portafolio/src/TailwindComponents.js
@@ -0,0 +1,43 @@
+const TailwindComponents = {
+  Boton: `
+  py-2 px-8 rounded-[13px] 
+  text-md font-semibold
+  dark:bg-[#323232] dark:text-white
+  bg-white text-zinc-500
+  hover:font-medium
+  hover:dark:bg-[#FFF] hover:dark:text-[#181818]
+  hover:bg-zinc-700 hover:text-white
+  transition duration-200 ease-out hover:ease-in
+  `,
+  BotonInactivo: `
+  pointer-events-none
+  py-2 px-6 rounded-lg 
+  bg-[#323232] text-white font-semibold
+  hover:bg-[#FFF] hover:text-[#181818] hover:font-medium
+  transition duration-200 ease-out hover:ease-in
+  `,
+  BotonLogo: `
+  rounded-lg
+  text-zinc-300 
+  dark:text-[#323232]
+  hover:text-white hover:animate-pulse
+  transition duration-200 ease-out hover:ease-in
+  `,
+  bgCard: `
+  p-4 place-content-center
+  text-black dark:text-white
+  bg-gradient-to-tl from-[#f5f5f5] to-[#e0e0e0] dark:from-[#121212] dark:to-[#1f1f1f]
+  rounded-[30px] border-t-[1px] border-l-[1px] dark:border-zinc-700 border-zinc-200
+  `,
+  BotonBgCard: `
+  p-4 place-content-center
+  text-black dark:text-white
+  hover:text-white hover:dark:text-black
+  bg-gradient-to-tl from-[#f5f5f5] to-[#e0e0e0] dark:from-[#121212] dark:to-[#1f1f1f]
+  hover:from-[#121212] hover:to-[#1f1f1f] hover:dark:from-[#f5f5f5] hover:dark:to-[#e0e0e0]
+  rounded-[30px] border-t-[1px] border-l-[1px] dark:border-zinc-700 border-zinc-200
+  `,
+
+};
+
+export default TailwindComponents;
